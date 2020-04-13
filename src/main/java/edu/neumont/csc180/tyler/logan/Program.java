15369ec3e8190fc9f97278584deefec70475e08b
LoganTyl <47304609+LoganTyl@users.noreply.github.com>
2020-04-13 14:37:35 -0600

    Removed html and pdf files

--- src/main/java/edu/neumont/csc180/tyler/logan/Program.java
+++ src/main/java/edu/neumont/csc180/tyler/logan/Program.java
@@ -4,7 +4,6 @@ public class Program {
     public static void main(String[] args) throws Exception {
         JsonToHtml jsonToHtml = new JsonToHtml();
         HtmlToPdf htmlToPdf = new HtmlToPdf();
-
         jsonToHtml.convertJsonToHtml();
         htmlToPdf.writeHtmlToPdf();
     }
